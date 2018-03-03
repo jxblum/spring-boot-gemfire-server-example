6913f74d0b124d822b6e294790688cb814499ee2
John Blum <jblum@pivotal.io>
2018-03-02 16:20:35 -0800

    Use Spring Data GemFire Annotation configuration to configure and bootstrap the Spring Boot, Pivotal GemFire Server.

--- src/main/java/org/example/SpringBootGemFireServer.java
+++ src/main/java/org/example/SpringBootGemFireServer.java
@@ -1,29 +1,24 @@
 package org.example;
 
-import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-
-import com.gemstone.gemfire.cache.Cache;
-import com.gemstone.gemfire.cache.CacheLoader;
-import com.gemstone.gemfire.cache.CacheLoaderException;
-import com.gemstone.gemfire.cache.LoaderHelper;
-import com.gemstone.gemfire.cache.RegionAttributes;
-import com.gemstone.gemfire.cache.server.CacheServer;
-import com.gemstone.gemfire.internal.DistributionLocator;
-
+import org.apache.geode.cache.Cache;
+import org.apache.geode.cache.CacheLoader;
+import org.apache.geode.cache.CacheLoaderException;
+import org.apache.geode.cache.LoaderHelper;
+import org.apache.geode.cache.RegionAttributes;
+import org.apache.geode.cache.server.CacheServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.gemfire.CacheFactoryBean;
 import org.springframework.data.gemfire.PartitionedRegionFactoryBean;
 import org.springframework.data.gemfire.RegionAttributesFactoryBean;
-import org.springframework.data.gemfire.server.CacheServerFactoryBean;
+import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
+import org.springframework.data.gemfire.config.annotation.EnableLocator;
+import org.springframework.data.gemfire.config.annotation.EnableManager;
+import org.springframework.data.gemfire.util.DistributedSystemUtils;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
 /**
  * A Spring Boot application bootstrapping a Pivotal GemFire Server in the JVM process.
@@ -32,84 +27,26 @@ import org.springframework.util.StringUtils;
  * @see org.springframework.boot.SpringApplication
  * @see org.springframework.boot.autoconfigure.SpringBootApplication
  * @see org.springframework.context.annotation.Bean
- * @see com.gemstone.gemfire.cache.Cache
- * @see com.gemstone.gemfire.cache.server.CacheServer
+ * @see org.apache.geode.cache.Cache
+ * @see org.apache.geode.cache.CacheLoader
+ * @see org.apache.geode.cache.server.CacheServer
  * @since 1.0.0
  */
 @SpringBootApplication
+@CacheServerApplication(name = "SpringBootGemFireServer")
+@EnableLocator
+@EnableManager
 @SuppressWarnings("all")
 public class SpringBootGemFireServer {
 
 	protected static final int DEFAULT_CACHE_SERVER_PORT = CacheServer.DEFAULT_PORT;
-	protected static final int DEFAULT_LOCATOR_PORT = DistributionLocator.DEFAULT_LOCATOR_PORT;
+	protected static final int DEFAULT_LOCATOR_PORT = DistributedSystemUtils.DEFAULT_LOCATOR_PORT;
 	protected static final int DEFAULT_MANAGER_PORT = 1099;
-
-	protected static final String DEFAULT_LOG_LEVEL = "config";
 
 	protected final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGemFireServer.class);
-	}
-
-	@Bean
-	Properties gemfireProperties(
-			@Value("${spring.gemfire.log-level:"+DEFAULT_LOG_LEVEL+"}") String logLevel,
-			@Value("${spring.gemfire.locators:localhost["+DEFAULT_LOCATOR_PORT+"]}") String locators,
-			@Value("${spring.gemfire.manager.port:"+DEFAULT_MANAGER_PORT+"}") int managerPort,
-			@Value("${spring.gemfire.manager.start:false}") boolean jmxManagerStart,
-			@Value("${spring.gemfire.start-locator}") String startLocator) {
-
-		logger.warn("spring.gemfire.log-level is [{}]", logLevel);
-
-		Properties gemfireProperties = new Properties();
-
-		gemfireProperties.setProperty("name", applicationName());
-		gemfireProperties.setProperty("mcast-port", "0");
-		gemfireProperties.setProperty("log-level", logLevel);
-		gemfireProperties.setProperty("locators", locators);
-		gemfireProperties.setProperty("jmx-manager", "true");
-		gemfireProperties.setProperty("jmx-manager-port", String.valueOf(managerPort));
-		gemfireProperties.setProperty("jmx-manager-start", String.valueOf(jmxManagerStart));
-
-		if (StringUtils.hasText(startLocator)) {
-			gemfireProperties.setProperty("start-locator", startLocator);
-		}
-
-		return gemfireProperties;
-	}
-
-	String applicationName() {
-		return SpringBootGemFireServer.class.getSimpleName();
-	}
-
-	@Bean
-	CacheFactoryBean gemfireCache(@Qualifier("gemfireProperties") Properties gemfireProperties) {
-
-		CacheFactoryBean gemfireCache = new CacheFactoryBean();
-
-		gemfireCache.setClose(true);
-		gemfireCache.setProperties(gemfireProperties);
-
-		return gemfireCache;
-	}
-
-	@Bean
-	CacheServerFactoryBean gemfireCacheServer(Cache gemfireCache,
-			@Value("${spring.gemfire.cache.server.bind-address:localhost}") String bindAddress,
-			@Value("${spring.gemfire.cache.server.hostname-for-clients:localhost}") String hostnameForClients,
-			@Value("${spring.gemfire.cache.server.port:"+DEFAULT_CACHE_SERVER_PORT+"}") int port) {
-
-		CacheServerFactoryBean gemfireCacheServer = new CacheServerFactoryBean();
-
-		gemfireCacheServer.setAutoStartup(true);
-		gemfireCacheServer.setCache(gemfireCache);
-		gemfireCacheServer.setBindAddress(bindAddress);
-		gemfireCacheServer.setHostNameForClients(hostnameForClients);
-		gemfireCacheServer.setMaxTimeBetweenPings(Long.valueOf(TimeUnit.SECONDS.toMillis(15)).intValue());
-		gemfireCacheServer.setPort(port);
-
-		return gemfireCacheServer;
 	}
 
 	@Bean(name = "Factorials")
